49d841b7f100d437ed3411242805e20c7662d383
Shahiduddin1710 <shahiduddin153@gmail.com>
2026-07-07 09:24:47 +0530

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import * as admin from 'firebase-admin';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,6 +8,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
